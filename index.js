c02f966381402a93dd5c1552e651044c42426b44
Bob Matcuk <bmatcuk@gmail.com>
2018-12-07 06:58:08 -1000

    updated dependencies

--- index.js
+++ index.js
@@ -7,39 +7,42 @@ _ = {
 };
 
 PugBrunchStatic = (function() {
-  function PugBrunchStatic(config1) {
-    var ref, ref1;
-    this.config = config1;
-    if ((ref = this.config) != null ? ref.fileMatch : void 0) {
-      this.handles = this.config.fileMatch;
-      delete this.config.fileMatch;
+  class PugBrunchStatic {
+    constructor(config1) {
+      var ref, ref1;
+      this.config = config1;
+      if ((ref = this.config) != null ? ref.fileMatch : void 0) {
+        this.handles = this.config.fileMatch;
+        delete this.config.fileMatch;
+      }
+      if ((ref1 = this.config) != null ? ref1.fileTransform : void 0) {
+        this.transformPath = this.config.fileTransform;
+        delete this.config.fileTransform;
+      }
     }
-    if ((ref1 = this.config) != null ? ref1.fileTransform : void 0) {
-      this.transformPath = this.config.fileTransform;
-      delete this.config.fileTransform;
+
+    transformPath(filename) {
+      return filename.replace(/\.static\.pug$/, '.html');
     }
-  }
+
+    compile(data, filename, options, context, callback) {
+      var opts, template;
+      opts = _.merge({}, {
+        filename: filename
+      }, this.config, options != null ? options.pug : void 0);
+      template = pug.compile(data, opts);
+      return callback(null, template(context));
+    }
+
+  };
 
   PugBrunchStatic.prototype.handles = /\.static\.pug$/;
 
-  PugBrunchStatic.prototype.transformPath = function(filename) {
-    return filename.replace(/\.static\.pug$/, '.html');
-  };
-
   PugBrunchStatic.prototype.acceptsContext = true;
-
-  PugBrunchStatic.prototype.compile = function(data, filename, options, context, callback) {
-    var opts, template;
-    opts = _.merge({}, {
-      filename: filename
-    }, this.config, options != null ? options.pug : void 0);
-    template = pug.compile(data, opts);
-    return callback(null, template(context));
-  };
 
   return PugBrunchStatic;
 
-})();
+}).call(this);
 
 module.exports = function(config) {
   return new PugBrunchStatic(config);
